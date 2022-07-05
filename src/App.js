fc432effc1e2b7b5132faf84f60e6f200a4c5043
Jessy Cristina <jessyaraujo395@gmail.com>
2022-07-05 18:59:32 -0300

    Requisito 1 concluido

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
-// começando o projeto
+import { Link, BrowserRouter } from 'react-router-dom';
+import Conteudo from './components/Conteudo';
+
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+
+        <Link to="/">Login</Link>
+        <Link to="/search">Search</Link>
+        <Link to="/album/:id">Album</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/profile/edit">ProfileEdit</Link>
+
+        <Conteudo />
+      </BrowserRouter>
+
+    );
   }
 }
 
---
+++ src/components/Conteudo.js
@@ -0,0 +1,31 @@
+// Funcionalidades
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// Paginas
+import Login from '../pages/Login';
+import Search from '../pages/Search';
+import Album from '../pages/Album';
+import Favorites from '../pages/Favorites';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
+import NotFound from '../pages/NotFound';
+
+class Conteudo extends React.Component {
+  render() {
+    return (
+
+      <Switch>
+        <Route path="/" exact component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+
+    );
+  }
+}
+
+export default Conteudo;
