272c88fcfe1e02ff544d1189ee60907ae11b2651
Jessy Cristina <jessyaraujo395@gmail.com>
2022-07-10 22:06:24 -0300

    requisito 8 concluido

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,32 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
 
 class MusicCard extends React.Component {
-  render() {
-    const { resultados, artista } = this.props;
-    return (
-      <div>
-        <h2 data-testid="artist-name">{artista.artistName}</h2>
-        <h3 data-testid="album-name">{artista.collectionName}</h3>
-        {resultados.map((info, index) => {
-          if (index > 0) {
-            return (
-              <div key={ info.trackName }>
-                <p>{info.trackName}</p>
-                <audio data-testid="audio-component" src={ info.previewUrl } controls>
-                  <track kind="captions" />
-                  O seu navegador não suporta o elemento
-                  {' '}
-                  {' '}
-                  <code>audio</code>
-                  .
-                </audio>
-              </div>);
-          } return undefined;
-        })}
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      carregando: false,
+    };
   }
+
+    handleClick = async () => {
+      this.setState({ carregando: true });
+      const { resultados } = this.props;
+      await addSong(resultados);
+      this.setState({ carregando: false });
+    }
+
+    render() {
+      const { resultados, artista } = this.props;
+      const { carregando } = this.state;
+      return (
+        <div>
+          {carregando && <Carregando />}
+          <h2 data-testid="artist-name">{artista.artistName}</h2>
+          <h3 data-testid="album-name">{artista.collectionName}</h3>
+          {resultados.map((info, index) => {
+            if (index > 0) {
+              return (
+                <div key={ info.trackName }>
+                  <p>{info.trackName}</p>
+                  <audio data-testid="audio-component" src={ info.previewUrl } controls>
+                    <track kind="captions" />
+                    O seu navegador não suporta o elemento
+                    {' '}
+                    {' '}
+                    <code>audio</code>
+                    .
+                  </audio>
+                  <label
+                    htmlFor={ info.trackName }
+                    data-testid={ `checkbox-music-${info.trackId}` }
+                  >
+                    favorita
+                    <input
+                      type="checkbox"
+                      id={ info.trackName }
+                      onClick={ this.handleClick }
+                    />
+                  </label>
+
+                </div>);
+            } return undefined;
+          })}
+        </div>
+      );
+    }
 }
 
 MusicCard.propTypes = {
